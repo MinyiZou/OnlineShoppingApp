e9706a456a529ea24504e0ef849bbf29fbeb3c88
MinyiZou <suusatoshigi@icloud.com>
2023-09-12 02:50:39 -0700

    feat: add models for the project

---
+++ MallApp/Model/Deal.swift
@@ -0,0 +1,13 @@
+//
+//  Deal.swift
+//  MallApp
+//
+//  Created by zouminyi on 9/12/23.
+//
+
+import Foundation
+
+struct Deal {
+    var product: Product
+    var progress: Int
+}
---
+++ MallApp/Model/Product.swift
@@ -0,0 +1,22 @@
+//
+//  Product.swift
+//  MallApp
+//
+//  Created by zouminyi on 9/12/23.
+//
+
+import Foundation
+
+struct Product {
+    var imageUrl: String
+    var name: String
+    var desc: String
+    var price: Int
+    var teacher: String
+    var total: Int
+    var update: Int
+    var studentCount: Int
+    var detail: String
+    var courseList: String
+}
+
